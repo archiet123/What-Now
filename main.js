1fc0298244fac3bfa9cf3aa09bfda45f00d4513f
archiet123 <114408893+archiet123@users.noreply.github.com>
2023-11-26 14:36:25 +0000

    basic login ui

--- main.js
+++ main.js
@@ -4,11 +4,12 @@ const createWindow = () => {
   const win = new BrowserWindow({ show: false })
   win.maximize();
   win.show();
-  win.loadURL('https://github.com')
 
-  const contents = win.webContents
-  console.log(contents)
-  // win.loadFile('index.html')
+  // win.loadURL('https://github.com')
+  // const contents = win.webContents
+  // console.log(contents)
+
+  win.loadFile('index.html')
 }
 
 app.whenReady().then(() => {
