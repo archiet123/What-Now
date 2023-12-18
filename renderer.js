d23ae74d88d698579f97200b5277a65777247ff0
Archie Tilling (student) <314988@edu.mkcollege.ac.uk>
2023-12-18 14:17:25 +0000

    using json as temp user auth

--- renderer.js
+++ renderer.js
@@ -1,17 +1,29 @@
-document.addEventListener('DOMContentLoaded', async () => {
-    console.log("Renderer > DOMContentLoaded");
+// document.addEventListener('DOMContentLoaded', async () => {
+//     console.log("Renderer > DOMContentLoaded");
 
-    let Names = window.Test.doThing()
+//     let Names = window.Test.doThing()
 
-    let DivNames = document.getElementById("test");
-    let String = Names.map((elem) => {
-        return elem.Name;
-    }).join("<br/>");
-    DivNames.innerText = String;
-});
+//     let DivNames = document.getElementById("test");
+//     let String = Names.map((elem) => {
+//         return elem.Name;
+//     }).join("<br/>");
+//     DivNames.innerText = String;
+// });
 
 function ColourSwitch() {
     document.body.classList.toggle("DarkScheme");
+}
+
+function SubmitClicked(){
+    
+    const Name = document.getElementById("fname").value;
+
+    if (Name != ''){
+        console.log(Name);
+    }else{
+        console.log("Error, please enter name");
+    }
+    
 }
 
 
